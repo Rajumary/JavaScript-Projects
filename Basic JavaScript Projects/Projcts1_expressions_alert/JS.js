55e485df223da352de736d4481802d118d5f94b3
Rajumary <rajumaryr@gmail.com>
2024-12-14 21:59:36 -0700

    Basic JavaScript assignment

---
+++ Basic JavaScript Projects/Projcts1_expressions_alert/JS.js
@@ -0,0 +1,14 @@
+// Declare a variable Sent1 and assign a string value to it
+let Sent1 = "This is the beginning of the string";
+
+// Declare a variable Sent2 and assign a string value to it
+let Sent2 = " and this is the end of the string";
+
+// Concatenate Sent1 and Sent2 into a single string and store it in a new variable
+let concatenatedString = Sent1 + Sent2;
+
+// Display the concatenated string in an alert box
+window.alert(concatenatedString);  // This will pop up an alert with the concatenated string
+
+// Display the concatenated string directly on the webpage using document.write
+document.write(concatenatedString);  // This will write the concatenated string to the webpage
